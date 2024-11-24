9d46365760d6d2dfd8e4482ab7bd69697a5417f8
devpresss xmont <xmontemorjerald@gmail.com>
2024-11-24 23:41:29 +0800

    Added forgotpassword form design, close button in login form fixed the bug in login passwordbox

--- ViewModel/LoginPage.xaml.cs
+++ ViewModel/LoginPage.xaml.cs
@@ -36,5 +36,154 @@ namespace GymManagementSystem.ViewModel
                DragMove();  
             }
         }
+
+      
+
+        private void Passwordtxt_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (Passwordtxt.Password.Length > 0) // Check if the password field is not empty
+            {
+                Eye.Visibility = Visibility.Visible; // Show the eye icon
+                PasswordErrorMessage.Text = "";
+                Passwordtxt.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#727272"));
+                Passwordtxt.BorderThickness = new Thickness(0, 0, 0, 2);
+            }
+            else
+            {
+                Eye.Visibility = Visibility.Collapsed; // Hide the eye icon
+            }
+        }
+         
+        private void Eye_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if(Passwordtxt.Visibility== Visibility.Visible)
+            {
+                PasswordUnmask.Text = Passwordtxt.Password;
+                PasswordUnmask.Visibility = Visibility.Visible;
+                Passwordtxt.Visibility = Visibility.Hidden;
+
+            }
+            else
+            {
+
+                PasswordUnmask.Visibility = Visibility.Collapsed;
+
+                Passwordtxt.Visibility = Visibility.Visible;
+
+            }
+
+        }
+
+        private void login()
+        {
+
+            string username= Usernametxt.Text;
+
+            string password= Passwordtxt.Password;
+
+
+            if (string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+            {
+
+                MessageBox.Show("All fields are required!!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(username))
+                {
+
+                    UsernameErrorMessage.Text = "Username is required";
+                    Usernametxt.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                    Usernametxt.BorderThickness = new Thickness(0, 0, 0, 2);
+
+                }
+                else if ((string.IsNullOrEmpty(password)))
+                {
+                    PasswordErrorMessage.Text = "Password is required";
+                    Passwordtxt.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                    Passwordtxt.BorderThickness = new Thickness(0, 0, 0, 2);
+                }
+                else
+                {
+
+                    if(username== "Admin" && password == "123")
+                    {
+
+
+                        MainWindow checkin = new MainWindow();
+                        checkin.Show();
+                        this.Hide();
+                    }
+                    else {
+                        MessageBox.Show("Incorrect username or password!!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        ClearTextBoxes();
+                    }
+
+
+                }
+
+
+
+            }
+
+
+
+
+        }
+
+      
+ 
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            login();
+        }
+
+        private void Usernametxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Usernametxt.Text.Length > 0)
+            {
+
+                UsernameErrorMessage.Text = "";
+                Usernametxt.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#727272"));
+                Usernametxt.BorderThickness = new Thickness(0, 0, 0, 2); 
+
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+
+            Usernametxt.Text = "";
+            Passwordtxt.Password = "";
+
+            PasswordUnmask.Visibility = Visibility.Hidden;
+            Passwordtxt.Visibility = Visibility.Visible;
+            
+
+        }
+
+        private void Unhide()
+        {
+
+
+        }
+
+        private void forgotpassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+
+            ForgotPassword forgotPassword = new ForgotPassword();
+            forgotPassword.Show();
+            this.Hide();
+        }
+
+        private void PackIconMaterial_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
--- obj/Debug/net8.0-windows/Login.g.i.cs
+++ obj/Debug/net8.0-windows/Login.g.i.cs
@@ -53,7 +53,7 @@ namespace GymManagementSystem {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GymManagementSystem;V1.0.0.0;component/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GymManagementSystem;component/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
